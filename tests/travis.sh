663079bb4b4e6cfce8a9b8b37b3b2bf3bc5a5905
Gordon Franke <info@nevalon.de>
2011-12-09 14:50:38 +0100

    try another way to install mysqlnd_uh extension

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,6 +1,13 @@
-sudo apt-get -y install php5-dev php-pear
-sudo pecl install mysqlnd_uh-alpha
-echo "extension=mysqlnd_ud.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+php -i
+
+wget http://pecl.php.net/get/mysqlnd_uh-0.1.0a1.tgz
+tar -xzf mysqlnd_uh-0.1.0a1.tgz
+sh -c "cd memcache-0.1.0a1 && phpize && ./configure && make && sudo make install"
+
+#sudo apt-get -y install php5-dev php-pear
+#sudo pecl install mysqlnd_uh-alpha
+
+echo "extension=myslnd_uh.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 wget http://getcomposer.org/composer.phar 
 php composer.phar install
