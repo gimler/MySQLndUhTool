f30b39abab471851d98f066b7ba38994f6caaf01
Gordon Franke <info@nevalon.de>
2011-12-09 11:42:28 +0100

    tarvis fix mysqlnd_ud installation again

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,5 +1,6 @@
-sudo apt-get -y install php5-dev 
+sudo apt-get -y install php5-dev php-pear
 sudo pecl install mysqlnd_uh-alpha
+echo "extension=mysqlnd_ud.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
 wget http://getcomposer.org/composer.phar 
 php composer.phar install
